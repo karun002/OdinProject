187ac88b17fdb340629db6f60fcd25ee8905742d
Karun Kumar <karunkumar@csus.edu>
2022-01-08 22:52:36 -0800

    rock papper scissor

---
+++ Fundamentals/JavaScript/rock_paper_scissors/source_code.js
@@ -0,0 +1,15 @@
+// Random integer between 0 and input
+function rand(num) {
+    
+}
+
+// Compare player entry and computer choice
+function computerPlay() {
+
+
+}
+
+// Compare player entry and computer choice
+function playRound() {
+
+}
